0aaac4321651ddc0f25cb78cb83cae47de8aa161
utsav@kumar <15102004utsavkumar@gmail.com>
2025-11-09 21:11:44 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -52,9 +52,10 @@ if (process.env.NODE_ENV === "production") {
   
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/:path(.*)", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+  app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
 }
 
 async function initDB() {
